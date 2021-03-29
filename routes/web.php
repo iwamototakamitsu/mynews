c4f1d2d8e41dbbb2878747358a8fd9047618d50b
EC2 Default User <ec2-user@ip-172-31-42-13.us-east-2.compute.internal>
2021-03-29 12:27:21 +0000

    課題09

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,13 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create', 'Admin\NewsController@add');
+    Route::get('profile/create ', 'ProfileController@add');
+    Route::get('profile/edit', 'ProfileController@edit');
+});
+
+
+//Route::get('XXX', 'AAAController@bbb');
+
+
